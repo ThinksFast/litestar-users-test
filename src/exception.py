9039127f17e5504818755927326cf71758f94349
ThinksFast <114229148+ThinksFast@users.noreply.github.com>
2023-08-25 11:05:02 -0600

    Added 500 error page to the exception handler
    `Litestar-users` throws an attribute error when logging into the app: `AttributeError: 'InstrumentedList' object has no attribute 'created_at'`
    
    While the session is created successfully and users can view protected routes, this also throws a 500 response and users get redirected to a 500 page upon login.

--- src/exception.py
+++ src/exception.py
@@ -2,10 +2,8 @@
 from typing import Callable
 
 from litestar import Request
-from litestar.response import Redirect
-from litestar.status_codes import (
-    HTTP_401_UNAUTHORIZED,
-)
+from litestar.response import Redirect, Template
+from litestar.status_codes import HTTP_401_UNAUTHORIZED, HTTP_500_INTERNAL_SERVER_ERROR
 
 
 def http_401(request: Request, exc: Exception) -> Redirect:
@@ -15,6 +13,16 @@ def http_401(request: Request, exc: Exception) -> Redirect:
     return Redirect(path="/login", status_code=302)
 
 
+def http_500(request: Request, exc: Exception) -> Template:
+    """Something broke, lets fix it"""
+    request.logger.exception(f"🚨 500 ERROR: something broke on {request.url}: {exc}")
+
+    return Template(
+        template_name="500.html.j2",
+    )
+
+
 exception_config: dict[int | type[Exception], Callable] = {
-    HTTP_401_UNAUTHORIZED: http_401
+    HTTP_401_UNAUTHORIZED: http_401,
+    HTTP_500_INTERNAL_SERVER_ERROR: http_500,
 }
